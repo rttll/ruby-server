62c53d7d8f4aad2f4627556ee066f10f10b6bf26
Adro <acousens@gmail.com>
2022-05-29 01:08:26 -0700

    User Rack to hand request/responses

--- app/server.rb
+++ app/server.rb
@@ -1,86 +1,60 @@
-# mirth.rb
-# HTTP Requests:
-# Break down HTTP request from the client 
-# and displays it 
-
-require 'socket'
+require 'rack'
+require 'rack/handler/puma'
 require 'erb'
 require 'yaml/store'
 
-port = 1337
-server = TCPServer.new(port)
+app = -> environment {
 
-Dir.mkdir('db') unless Dir.exist? 'db'
+  Dir.mkdir('db') unless Dir.exist? 'db'
+  store = YAML::Store.new("db/db.yml")
+  store.transaction do
+    store[:birthdays] = [] if store[:birthdays].nil?
+  end
+  
+  routes = {
+    '/' => 'index'
+  }
+  
+  @birthdays = []
+  
+  def template(file)
+    file = File.read("#{Dir.pwd}/app/views/#{file}.html.erb")
+    erb = ERB.new file
+    erb.result(binding)  
+  end
 
-store = YAML::Store.new("db/db.yml")
-store.transaction do
-  store[:birthdays] = [] if store[:birthdays].nil?
-end
+  request = Rack::Request.new(environment)
+  
+  status = 200
+  content_type = 'text/html'
+  response_message = '' 
 
-routes = {
-  '/' => 'index'
-}
-
-@birthdays = []
-def template(file)
-  file = File.read("#{Dir.pwd}/app/views/#{file}.html.erb")
-  erb = ERB.new file
-  erb.result(binding)  
-end
-
-loop do
-  client = server.accept
-
-  request_line = client.readline
-
-  method_token, target, version_number = request_line.split 
-  response_status_code="200 OK"
-  content_type ="text/html"
-  response_message = ""
-
-  puts "incoming: #{method_token} #{target} on port #{port}"
-
-  case [method_token, target]
-  when ['GET', '/']
-
+  if request.get? && request.path == '/'  
     store.transaction do
       @birthdays = store[:birthdays]
     end
     
-    file = routes[target]
+    file = routes[request.path]
     response_message << template(file)
 
-  when ['POST', '/birthdays']
-    response_status_code = "303 See Other"
-    
-    all_headers = {}
-    while true
-      line = client.readline 
-      break if line == "\r\n"
-      header_name, value = line.split(": ")
-      all_headers[header_name] = value
-    end
-    body = client.read(all_headers['Content-Length'].to_i)
-    require 'uri'
-    new_birthday = URI.decode_www_form(body).to_h
+  elsif request.post? && request.path == '/birthdays'
+    status = 303
+    new_birthday = request.params
     store.transaction do
       store[:birthdays] << new_birthday.transform_keys(&:to_sym)
     end
 
   else
-    content_type ="text/plain"
-    response_message = "✅ Received a #{method_token} request to #{target} with #{version_number}"
+    content_type = 'text/plain'
+    response_message = "✅ Received a #{request.request_method} request to #{request.path}"
   end
 
-  http_response = <<~MSG
-    #{version_number} #{response_status_code}
-    Content-Type: #{content_type}; charset=#{response_message.encoding.name}
-    Location: /
+  headers = { 
+    'Content-Type' => "#{content_type}; charset=#{response_message.encoding.name}", 
+    "Location" => "/" 
+  }
+  body = [response_message]
+  [status, headers, body]
+}
 
-    #{response_message}
-  MSG
-
-  client.puts http_response
-  client.close
-
-end
+Rack::Handler::Puma.run(app, :Port => 1337, :Verbose => true) 
